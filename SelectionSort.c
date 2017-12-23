effd358d275736e6f32a50876860d85bd25dd16d
JosephGormley <josephbgormley@gmail.com>
2017-12-22 20:00:55 -0500

    Initial Commit
    N/A

---
+++ SelectionSort.c
@@ -0,0 +1,14 @@
+/* Author: Joseph Gormley - Date: 12/22/17 */
+#include <stdio.h>
+
+int main(){
+	
+	// List of 15 strings randomly selected from https://www.randomlists.com
+	char * list[15] = {"mind", "duck", "slow", "precious", "joke",
+				  "sign", "ask", "special", "silky", "ruthless",
+				  "rainy", "delirious", "scare", "van", "join"};
+	
+	printf("%s\n", list[1]);
+	
+	return 0;
+}
