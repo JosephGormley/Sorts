526860b801406d8a495f1af204b9f36e905169b2
JosephGormley <josephbgormley@gmail.com>
2017-12-25 15:05:42 -0500

    Bug fixes, tested and works.
    N/a

--- SelectionSort.c
+++ SelectionSort.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 
-// Number of comparisons Selection Sort took to sort this list. 
+// Number of comparisons used to sort this list. 
 int numberOfComparisons = 0;
 
 // Function declarations.
@@ -20,47 +20,63 @@ int main(){
 	char * list[LIST_SIZE] = {"mind", "duck", "slow", "precious", "joke",
 				  "sign", "ask", "special", "silky", "ruthless",
 				  "rainy", "delirious", "scare", "van", "join"};
-	
-	// Print list, sort list, print list again. 
+
+	// Print list
+	printf("Unsorted list\n");
+	printf("-------------\n"); 
 	printList(list);
+
+	// Sort List
 	sort(list);
+
+	// Print list again. 
+	printf("Sorted list\n");
+	printf("-----------\n"); // For presentation. 
 	printList(list);
+	printf("Comparisons: %d\n\n", numberOfComparisons);
 
 	return 0;
 }
 
-// SReqrqr
+// This function sorts a list of strings with selection sort. 
 void sort(char ** list){
 
+	int indexToSwap = -1; // This variable is used to swap the words. 
+	char * tmp = NULL; // This variable is used to swap the words. 
 	char * wordToSwap = list[0];
 
-	// Find element that belongs in properIndex's location. 
+	// Find string that belongs in properIndex's location. 
 	int properIndex;
-	for(properIndex = 0; properIndex < LIST_SIZE - 1; properIndex++){
+	for(properIndex = 0; properIndex  < LIST_SIZE; properIndex++){
+		
+		wordToSwap = list[properIndex];
 
-		// Find smallest element 
+		// Find smallest string 
 		int i;
-		for(i = properIndex; i < LIST_SIZE - 1; i++){
+		for(i = properIndex + 1; i < LIST_SIZE; i++){
 			numberOfComparisons++;
 			// Is wordToSwap smaller than list[i]?
-			if(strcmp(wordToSwap, list[i]) < 0){
+			if(strcmp(wordToSwap, list[i]) >= 0){
 				wordToSwap = list[i];
+				indexToSwap = i; 
 			} 
 		}
 
+		// Swap words. 
+		tmp = list[properIndex];
 		list[properIndex] = wordToSwap;
-
+		list[indexToSwap] = tmp;
+		
 	}
 
 	return;
 }
 
-/* Prints out a given list of elements */
+/* Prints out a given list of strings */
 void printList(char ** list){
 
-	printf("LIST:\n");
 
-	// Iterate list of elements. 
+	// Iterate list of strings. 
 	int i;
 	for(i = 0; i < LIST_SIZE; i++){
 		printf("%s\n", list[i]);
